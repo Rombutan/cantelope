4c5310357b2b39e01a104a7a3989ecaba110510c
rombutan <lucaguyyounes@gmail.com>
2026-01-25 21:23:36 -0800

    move candumpparse to seperate library

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,21 @@ use dbc_rs::Dbc;
 use std::fs;
 
 // Arrow IP elements
+use arrow::datatypes::{DataType, Field, Schema};
 use std::sync::Arc;
-use arrow::datatypes::{DataType, Schema, Field};
 
 // Literally only for cleaner print
 use std::io::{self, Write};
 
 // Custom data storage helpers
 pub mod store;
-use store::{GenericColumn, Column};
+use store::{Column, GenericColumn};
 
 // Custom argument parsing
-pub mod args; 
+pub mod args;
 
 // Custom Candump parsing
-pub mod candumpparse;
-
+use candump::CanDumpParser;
 
 // Used for type decisions only
 trait FloatExt {
@@ -27,14 +26,14 @@ trait FloatExt {
 
 impl FloatExt for f64 {
     fn is_nearly(&self, target: f64) -> bool {
-        // Use a slightly larger margin than f64::EPSILON 
+        // Use a slightly larger margin than f64::EPSILON
         // if you expect multiple cumulative calculations.
         (self - target).abs() < f64::EPSILON
     }
 }
 
 fn main() {
-    let args = args::process_args(); // Load arguments into a struct 
+    let args = args::process_args(); // Load arguments into a struct
 
     let dbc_content = fs::read_to_string(&args.dbcfile).unwrap(); // Load DBC file contents into string
     let dbc = Dbc::parse(&dbc_content).unwrap(); // Parse DBC
@@ -55,53 +54,58 @@ fn main() {
 
     is_filled.push(true); // This element of the map won't actually be used, but is needed for indecies to align
 
-    for message in dbc.messages().iter(){
-        for signal in message.signals().iter(){
+    for message in dbc.messages().iter() {
+        for signal in message.signals().iter() {
             is_filled.push(false); // If I ever update this to exclude ANY signals which are present in the DBC, I will need to move this into the blocks below
-            if  signal.length() == 1  
-                && signal.is_unsigned() 
-                && signal.factor().is_nearly(1.0) 
-                && signal.offset().is_nearly(0.0) { // Definetely a boolean
+            if signal.length() == 1
+                && signal.is_unsigned()
+                && signal.factor().is_nearly(1.0)
+                && signal.offset().is_nearly(0.0)
+            {
+                // Definetely a boolean
                 base_row_size += 1;
                 fields.push(Field::new(signal.name(), DataType::Boolean, true));
                 columns.push(GenericColumn::Bool(Column::new()));
-            
-            } else if (signal.factor() % 1.0).is_nearly(1.0) {   // Definetely an integer
-                if signal.min() >= f64::from(i8::MIN) && signal.max() <= f64::from(i8::MAX) {    // Fits in i8
-                    base_row_size+=8;
+            } else if (signal.factor() % 1.0).is_nearly(1.0) {
+                // Definetely an integer
+                if signal.min() >= f64::from(i8::MIN) && signal.max() <= f64::from(i8::MAX) {
+                    // Fits in i8
+                    base_row_size += 8;
                     fields.push(Field::new(signal.name(), DataType::Int8, true));
                     columns.push(GenericColumn::I8(Column::new()));
-                
-                } else if signal.min() >= f64::from(i16::MIN) && signal.max() <= f64::from(i16::MAX) {  // Fits in i16 
-                    base_row_size+=16;
+                } else if signal.min() >= f64::from(i16::MIN) && signal.max() <= f64::from(i16::MAX)
+                {
+                    // Fits in i16
+                    base_row_size += 16;
                     fields.push(Field::new(signal.name(), DataType::Int16, true));
                     columns.push(GenericColumn::I16(Column::new()));
-
-                } else if signal.min() >= f64::from(i32::MIN) && signal.max() <= f64::from(i32::MAX) {  // Fits in i32
-                    base_row_size+=32;
+                } else if signal.min() >= f64::from(i32::MIN) && signal.max() <= f64::from(i32::MAX)
+                {
+                    // Fits in i32
+                    base_row_size += 32;
                     fields.push(Field::new(signal.name(), DataType::Int32, true));
                     columns.push(GenericColumn::I32(Column::new()));
-                
-                }  else {  // must fits in i64 :shrug 
-                    base_row_size+=64;
+                } else {
+                    // must fits in i64 :shrug
+                    base_row_size += 64;
                     fields.push(Field::new(signal.name(), DataType::Int64, true));
                     columns.push(GenericColumn::I64(Column::new()));
                 }
-            
-            } else {    // Float
-//                if signal.min() >= f64::from(f16::MIN) && signal.max() <= f64::from(f16::MAX) {   // Fits in f16 (Currently only works in rust-unstable
-//                    print!("f16");
-//                    base_row_size+=16;
-//                    fields.push(Field::new(signal.name(), DataType::Float16, true));
-//                }
-                
+            } else {
+                // Float
+                //                if signal.min() >= f64::from(f16::MIN) && signal.max() <= f64::from(f16::MAX) {   // Fits in f16 (Currently only works in rust-unstable
+                //                    print!("f16");
+                //                    base_row_size+=16;
+                //                    fields.push(Field::new(signal.name(), DataType::Float16, true));
+                //                }
+
                 if signal.min() >= f64::from(f32::MIN) && signal.max() <= f64::from(f32::MAX) {
-                    base_row_size+=32;
+                    base_row_size += 32;
                     fields.push(Field::new(signal.name(), DataType::Float32, true));
                     columns.push(GenericColumn::F32(Column::new()));
-                
-                } else { // Must fits in f64 :shrug
-                    base_row_size+=64;
+                } else {
+                    // Must fits in f64 :shrug
+                    base_row_size += 64;
                     fields.push(Field::new(signal.name(), DataType::Float64, true));
                     columns.push(GenericColumn::F64(Column::new()));
                 }
@@ -112,48 +116,54 @@ fn main() {
     let schema = Arc::new(Schema::new(fields));
     // ------
 
-
-    let mut parser = candumpparse::CanDumpParser::new(&args.input).unwrap();
+    let mut parser = CanDumpParser::new(&args.input).unwrap();
 
     parser.parse(); // Data from first can packet ignored :(
     let time_start = parser.get_timestamp();
     let mut num_chunks = 0;
 
     let mut exit = false;
-    while !exit { // Message recieve loop
+    while !exit {
+        // Message recieve loop
         exit = parser.parse();
         //if exit {continue;} // Prevents continued execution resulting in duplicated values when file is over, breaks finishing of last row
 
-        let relative_time_rcv = (parser.get_timestamp() - time_start)*1000.0; // time since start of recording
+        let relative_time_rcv = (parser.get_timestamp() - time_start) * 1000.0; // time since start of recording
 
         match dbc.decode(parser.get_id(), &parser.get_data(), false) {
             Ok(decoded) => {
                 for signal in decoded.iter() {
                     let col = &mut columns[schema.index_of(signal.name).unwrap()];
-                    if !is_filled[schema.index_of(signal.name).unwrap()] { // Only save the first value from each chunk (as opposed to prev version saving last)
+                    if !is_filled[schema.index_of(signal.name).unwrap()] {
+                        // Only save the first value from each chunk (as opposed to prev version saving last)
                         match col {
                             GenericColumn::Bool(c) => c.push(Some(signal.value.is_nearly(1.0))),
                             GenericColumn::I8(c) => c.push(Some(signal.value as i8)),
                             GenericColumn::I32(c) => c.push(Some(signal.value as i32)),
                             GenericColumn::I64(c) => c.push(Some(signal.value as i64)),
-//                            GenericColumn::F16(c) => c.push(Some(f16::from(signal.value))),
+                            //                            GenericColumn::F16(c) => c.push(Some(f16::from(signal.value))),
                             GenericColumn::F32(c) => c.push(Some(signal.value as f32)),
                             GenericColumn::F64(c) => c.push(Some(signal.value)),
                             _ => {}
                         }
-                        is_filled[schema.index_of(signal.name).unwrap()] =  true;
+                        is_filled[schema.index_of(signal.name).unwrap()] = true;
                     }
                 }
             }
-            Err(e) => println!("Signal: {} Data: {:02x?}  Error: {}", parser.get_id(), parser.get_data(), e),
+            Err(e) => println!(
+                "Signal: {} Data: {:02x?}  Error: {}",
+                parser.get_id(),
+                parser.get_data(),
+                e
+            ),
             //Err(e) => _ = e,
         }
-        if relative_time_rcv > (&args.cache_ms * f64::from(num_chunks)) || exit{
+        if relative_time_rcv > (&args.cache_ms * f64::from(num_chunks)) || exit {
             let col = &mut columns[schema.index_of("Time_ms").unwrap()];
             is_filled[schema.index_of("Time_ms").unwrap()] = true;
             match col {
-                    GenericColumn::F64(c) => c.push(Some(relative_time_rcv)),
-                    _ => {}
+                GenericColumn::F64(c) => c.push(Some(relative_time_rcv)),
+                _ => {}
             }
             let mut empty_cols = 0;
             for (index, value) in is_filled.iter().enumerate() {
--- src/store.rs
+++ src/store.rs
@@ -4,9 +4,9 @@ use arrow::array::*;
 use arrow::datatypes::*;
 use arrow::record_batch::RecordBatch;
 
-use std::fs::File;
 use parquet::arrow::ArrowWriter;
 use parquet::file::properties::WriterProperties;
+use std::fs::File;
 
 pub struct Column<T> {
     values: Vec<T>,
@@ -43,7 +43,7 @@ pub enum GenericColumn {
     Bool(Column<bool>),
     F64(Column<f64>),
     F32(Column<f32>),
-//    F16(Column<f16>),
+    //    F16(Column<f16>),
     I8(Column<i8>),
     I16(Column<i16>),
     I32(Column<i32>),
@@ -54,31 +54,31 @@ impl GenericColumn {
     pub fn push_null(&mut self) {
         match self {
             GenericColumn::Bool(c) => c.push(None),
-            GenericColumn::F64(c)  => c.push(None),
-            GenericColumn::F32(c)  => c.push(None),
-            GenericColumn::I8(c)   => c.push(None),
-            GenericColumn::I16(c)  => c.push(None),
-            GenericColumn::I32(c)  => c.push(None),
-            GenericColumn::I64(c)  => c.push(None),
+            GenericColumn::F64(c) => c.push(None),
+            GenericColumn::F32(c) => c.push(None),
+            GenericColumn::I8(c) => c.push(None),
+            GenericColumn::I16(c) => c.push(None),
+            GenericColumn::I32(c) => c.push(None),
+            GenericColumn::I64(c) => c.push(None),
         }
     }
 
     pub fn data_type(&self) -> DataType {
         match self {
             GenericColumn::Bool(_) => DataType::Boolean,
-            GenericColumn::I8(_)   => DataType::Int8,
-            GenericColumn::I16(_)  => DataType::Int16,
-            GenericColumn::I32(_)  => DataType::Int32,
-            GenericColumn::I64(_)  => DataType::Int64,
-            GenericColumn::F32(_)  => DataType::Float32,
-            GenericColumn::F64(_)  => DataType::Float64,
+            GenericColumn::I8(_) => DataType::Int8,
+            GenericColumn::I16(_) => DataType::Int16,
+            GenericColumn::I32(_) => DataType::Int32,
+            GenericColumn::I64(_) => DataType::Int64,
+            GenericColumn::F32(_) => DataType::Float32,
+            GenericColumn::F64(_) => DataType::Float64,
         }
     }
 
     pub fn len(&self) -> usize {
         match self {
             GenericColumn::Bool(c) => c.len(),
-            GenericColumn::I8(c)  => c.len(),
+            GenericColumn::I8(c) => c.len(),
             GenericColumn::I16(c) => c.len(),
             GenericColumn::I32(c) => c.len(),
             GenericColumn::I64(c) => c.len(),
@@ -90,7 +90,7 @@ impl GenericColumn {
     pub fn finish(self) -> Arc<dyn Array> {
         match self {
             GenericColumn::Bool(c) => Arc::new(BooleanArray::from(c.values)),
-            GenericColumn::I8(c)  => Arc::new(Int8Array::from(c.values)),
+            GenericColumn::I8(c) => Arc::new(Int8Array::from(c.values)),
             GenericColumn::I16(c) => Arc::new(Int16Array::from(c.values)),
             GenericColumn::I32(c) => Arc::new(Int32Array::from(c.values)),
             GenericColumn::I64(c) => Arc::new(Int64Array::from(c.values)),
@@ -104,24 +104,14 @@ pub fn finish_record_batch(columns: Vec<GenericColumn>, schema: Arc<Schema>) -> 
     assert!(!columns.is_empty());
 
     let row_count = columns[0].len();
-    let mut i = 0;
-    
+
     for c in &columns {
-        println!("Column: {} len: {}", schema.field(i).name(), c.len());
-        i += 1; 
-    }
-    
-    for c in &columns {
-        println!("Column: {} len: {}", schema.field(i).name(), c.len()); 
         assert_eq!(c.len(), row_count, "column length mismatch");
     }
 
-    let arrays: Vec<Arc<dyn Array>> =
-        columns.into_iter().map(|c| c.finish()).collect();
+    let arrays: Vec<Arc<dyn Array>> = columns.into_iter().map(|c| c.finish()).collect();
 
-
-    RecordBatch::try_new(schema, arrays)
-        .expect("failed to create RecordBatch")
+    RecordBatch::try_new(schema, arrays).expect("failed to create RecordBatch")
 }
 
 pub fn write_record_batch_to_parquet(
@@ -134,11 +124,7 @@ pub fn write_record_batch_to_parquet(
         .set_compression(parquet::basic::Compression::SNAPPY)
         .build();
 
-    let mut writer = ArrowWriter::try_new(
-        file,
-        batch.schema(),
-        Some(props),
-    )?;
+    let mut writer = ArrowWriter::try_new(file, batch.schema(), Some(props))?;
 
     writer.write(batch)?;
     writer.close()?;
